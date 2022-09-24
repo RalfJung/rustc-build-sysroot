697f8eba89e3f1f9b5eb0224ab697e4a69bd1d5b
Ralf Jung <post@ralfj.de>
2022-09-24 17:02:20 +0200

    also test cross-builds

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,3 +1,4 @@
+use std::path::Path;
 use std::process::{self, Command};
 
 use rustc_version::VersionMeta;
@@ -6,7 +7,36 @@ use tempdir::TempDir;
 use rustc_build_sysroot::*;
 
 fn run(cmd: &mut Command) {
-    assert!(cmd.status().expect("failed to run {cmd:?}").success());
+    assert!(cmd.stdout(process::Stdio::null()).stderr(process::Stdio::null()).status().expect("failed to run {cmd:?}").success());
+}
+
+fn test_sysroot_build(target: &str, mode: BuildMode, src_dir: &Path, rustc_version: &VersionMeta) {
+    let sysroot_dir = TempDir::new("rustc-build-sysroot-test-sysroot").unwrap();
+    let sysroot = Sysroot::new(sysroot_dir.path(), target);
+    sysroot
+        .build_from_source(src_dir, mode, rustc_version, || {
+            let mut cmd = Command::new("cargo");
+            cmd.stdout(process::Stdio::null());
+            cmd.stderr(process::Stdio::null());
+            cmd
+        })
+        .unwrap();
+
+    let crate_name = "rustc-build-sysroot-test-crate";
+    let crate_dir = TempDir::new(crate_name).unwrap();
+    run(Command::new("cargo")
+        .args(&["new", crate_name])
+        .current_dir(&crate_dir));
+    let crate_dir = crate_dir.path().join(crate_name);
+    run(Command::new("cargo")
+        .arg(mode.as_str())
+        .arg("--target")
+        .arg(target)
+        .current_dir(&crate_dir)
+        .env(
+            "RUSTFLAGS",
+            format!("--sysroot {}", sysroot_dir.path().display()),
+        ));
 }
 
 #[test]
@@ -15,29 +45,20 @@ fn host() {
     let src_dir = rustc_sysroot_src(Command::new("rustc")).unwrap();
 
     for mode in [BuildMode::Build, BuildMode::Check] {
-        let sysroot_dir = TempDir::new("rustc-build-sysroot-test-sysroot").unwrap();
-        let sysroot = Sysroot::new(sysroot_dir.path(), &rustc_version.host);
-        sysroot
-            .build_from_source(&src_dir, mode, &rustc_version, || {
-                let mut cmd = Command::new("cargo");
-                cmd.stdout(process::Stdio::null());
-                cmd.stderr(process::Stdio::null());
-                cmd
-            })
-            .unwrap();
+        test_sysroot_build(&rustc_version.host, mode, &src_dir, &rustc_version);
+    }
+}
 
-        let crate_name = "rustc-build-sysroot-test-crate";
-        let crate_dir = TempDir::new(crate_name).unwrap();
-        run(Command::new("cargo")
-            .args(&["new", crate_name])
-            .current_dir(&crate_dir));
-        let crate_dir = crate_dir.path().join(crate_name);
-        run(Command::new("cargo")
-            .arg(mode.as_str())
-            .current_dir(&crate_dir)
-            .env(
-                "RUSTFLAGS",
-                format!("--sysroot {}", sysroot_dir.path().display()),
-            ));
+#[test]
+fn cross() {
+    let rustc_version = VersionMeta::for_command(Command::new("rustc")).unwrap();
+    let src_dir = rustc_sysroot_src(Command::new("rustc")).unwrap();
+
+    for target in [
+        "i686-unknown-linux-gnu",
+        "aarch64-apple-darwin",
+        "i686-pc-windows-msvc",
+    ] {
+        test_sysroot_build(target, BuildMode::Check, &src_dir, &rustc_version);
     }
 }
