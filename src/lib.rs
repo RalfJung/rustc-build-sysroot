24ca97b7216511b9f18681e72a59cf872e4711a6
Ralf Jung <post@ralfj.de>
2022-09-28 22:05:27 +0200

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -268,8 +268,8 @@ path = {src_dir_workspace_std:?}
 
         // Copy the output to a staging dir (so that we can do the final installation atomically.)
         fs::create_dir_all(&self.sysroot_dir).context("failed to create sysroot dir")?; // TempDir expects the parent to already exist
-        let staging_dir = TempDir::new_in(&self.sysroot_dir)
-            .context("failed to create staging dir")?;
+        let staging_dir =
+            TempDir::new_in(&self.sysroot_dir).context("failed to create staging dir")?;
         let out_dir = build_target_dir
             .join(&self.target)
             .join("release")
